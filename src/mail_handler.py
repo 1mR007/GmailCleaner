c4fc0457fe8c4881b679436447080e20c444b7f1
Hossaïne <hossaine.far@gmail.com>
2025-05-01 16:43:03 +0200

    Update .gitignore, requirements, and refactor mail_handler and test scripts for Gmail API integration

--- src/mail_handler.py
+++ src/mail_handler.py
@@ -1,101 +1,321 @@
-# src/mail_handler.py
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 import config
 import utils
-import imaplib
-import email
 
-def gmail_connect():
-    """
-    Connect to Gmail using IMAP and return the connection object.
-    """
-    try:
-        mail = imaplib.IMAP4_SSL(config.IMAP_SERVER, config.IMAP_PORT)
-        mail.login(config.EMAIL_ADDRESS, config.APP_PASSWORD)
-        return mail
-    except Exception as e:
-        print(f"Error while logging to Gmail: {e}")
+SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
-def close_connection(mail):
-    """
-    Close the mail connection properly.
-    """
-    if mail:
-        try:
-            mail.close()
-            mail.logout()
-        except Exception as e:
-            print(f"Error during logout: {e}")
+def start_gmail_api():
+  """
+  Start the Gmail API by checking for existing credentials and refreshing them if necessary.
+  If no credentials are found, it prompts the user to log in and saves the credentials (token.json) for future use.
 
-def search_unseen_emails(mail, mode="UNSEEN", fallback_mode="ALL"):
-    """
-    Search for unseen emails in the inbox based on the mode. If no emails are found using the given mode,
-    it will search using the fallback mode.
+  Returns:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  If the authentication fails, it returns None.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  try:
+    if os.path.exists("token.json"):
+      creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+      print(f"Token loaded from {os.path.abspath('token.json')}")
+      if creds.valid:
+        print("Token is valid, Gmail API can be accessed.\n")
+        return creds
+    else:
+      print("Token file not found. Please authenticate.")
+  except ValueError as e:
+    print(f"Error loading token: {e}")
+  except FileNotFoundError as e:
+    print(f"Token file not found: {e}")
+  except json.JSONDecodeError as e:
+    print(f"Error decoding token JSON: {e}")
+  except TypeError as e:
+    print(f"Error loading token: {e}")
+  except Exception as e:
+    print(f"Error loading token: {e}")
+
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+        "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+      
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+      print(f"Token saved to {os.path.abspath('token.json')}")
+
+def test_gmail_api(creds):
+  """
+  Test the Gmail API by listing the user's labels.
+  This function is called after the user has authenticated and the token has been saved.
+  It uses the credentials to access the Gmail API and list the user's labels.
+  If the API call is successful, it prints the labels to the console.
+  
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  """
+  try:
     
-    Parameters:
-    - mode: Primary search mode (e.g., 'UNSEEN', 'ALL', etc.).
-    - fallback_mode: Fallback search mode if the primary search fails (default is 'ALL').
-    
-    Returns a list of email IDs.
-    """
-    try:
-        mail.select()
-        typ, data = mail.search(None, mode)
-        print(f"Search status: {typ}, Data: {data}")
-        if typ == 'OK' and data[0]:
-            if data[0] == b'':
-                print(f"No emails found using mode '{mode}'. Trying fallback mode '{fallback_mode}'...")
-                typ, data = mail.search(None, fallback_mode)
-            return data[0].split()
-        else:
-            print(f"Failed to search for emails using mode '{mode}'.")
-            return []
-    except Exception as e:
-        print(f"Error searching emails: {e}")
-        return []
-def fetch_email_by_id(connection, email_id):
-    """
-    Fetch an email by its ID.
-    """
-    try:
-        typ, data = connection.fetch(email_id, '(RFC822)')
-        if typ == 'OK':
-            return email.message_from_bytes(data[0][1])
-        else:
-            print(f"Failed to fetch email with ID {email_id}.")
-            return None
-    except Exception as e:
-        print(f"Error fetching email: {e}")
-        return None
-    
-def is_promotional(subject, sender):
-    """
-    Check if the email is promotional based on the subject and sender.
-    """
-    if not subject or not sender:
-        return False
-    
-    promotional_subjects_file = config.PROMOTIONAL_SUBJECTS_FILE
-    promotional_senders_file = config.PROMOTIONAL_SENDERS_FILE
-    promotional_domains_file = config.PROMOTIONAL_DOMAINS_FILE
+    if creds:
+      # Call the Gmail API
+      service = build("gmail", "v1", credentials=creds)
+      results = service.users().labels().list(userId="me").execute()
 
-    local_part = sender.split('@')[0]
-    domain_part = sender.split('@')[1] if '@' in sender else ''
+      print("-" * 20)
+      print("Gmail API is working !")
+      print("Trying to list labels...")
 
-    promotional_subjects = utils.read_file(promotional_subjects_file)
-    promotional_senders = utils.read_file(promotional_senders_file)
-    promotional_domains = utils.read_file(promotional_domains_file)
+      # Call the Gmail API to list labels
+      labels = results.get("labels", [])
 
-    # Check if the subject or sender matches any promotional criteria
-    for promo_subject in promotional_subjects:
-        if promo_subject.lower() in subject.lower():
-            return True
-    for promo_sender in promotional_senders:
-        if promo_sender.lower() in local_part.lower():
-            return True
-    for promo_domain in promotional_domains:
-        if promo_domain.lower() in domain_part.lower():
-            return True
-        
-    # If no matches found, return False 
+      if not labels:
+        print("No labels found.")
+        return
+      print("\nAll labels found :\n")
+      for label in labels:
+        print(label["name"])
+    else:
+      print("No valid credentials found. Please authenticate.")
+      return
+    
+  except FileNotFoundError as e:
+    print(f"File not found: {e}")
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f"An error occurred: {error}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
+def get_unread_emails(creds):
+  """
+  Get unread emails from the user's Gmail inbox using the Gmail API.
+  This function retrieves unread emails from the user's inbox and return the message IDs to the console.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().messages().list(userId="me", labelIds=["INBOX", "UNREAD"]).execute()
+    messages = results.get("messages", [])
+
+    return messages
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while getting unread emails : {error}")
+    return
+  except Exception as e:
+    print(f"An error occured while getting unread emails : {e}")
+    return
+  
+def get_sender_email(creds, message_id):
+  """
+  Get the sender's email address from a specific email message using the Gmail API.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  message_id (str): The ID of the email message to retrieve.
+
+  Returns:
+  str: The sender's email address if found, otherwise None.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    message = service.users().messages().get(userId="me", id=message_id).execute()
+    headers = message["payload"]["headers"]
+
+    for header in headers:
+      if header["name"] == "From":
+        sender = header["value"].split("<")[-1].split(">")[0]
+        return sender
+    return None
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while getting sender email : {error}")
+    return None
+  except Exception as e:
+    print(f"An error occurred while getting sender email : {e}")
+    return None
+  
+def get_email_subject(creds, message_id):
+  """
+  Get the subject of a specific email message using the Gmail API.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  message_id (str): The ID of the email message to retrieve.
+
+  Returns:
+  str: The subject of the email if found, otherwise None.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    message = service.users().messages().get(userId="me", id=message_id).execute()
+    headers = message["payload"]["headers"]
+
+    for header in headers:
+      if header["name"] == "Subject":
+        return header["value"]
+    return None
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while getting email subject : {error}")
+    return None
+  except Exception as e:
+    print(f"An error occurred while getting email subject : {e}")
+    return None
+  
+def get_email_domain(creds, message_id):
+  """
+  Get the domain of the sender's email address from a specific email message using the Gmail API.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  message_id (str): The ID of the email message to retrieve.
+
+  Returns:
+  str: The domain of the sender's email if found, otherwise None.
+  """
+  try:
+    sender = get_sender_email(creds, message_id)
+    if sender:
+      domain = sender.split("@")[-1].split(">")[0]
+      return domain
+    return None
+
+  except Exception as e:
+    print(f"An error occurred while getting email domain : {e}")
+    return None
+  
+def is_promo_email(creds, message_id):
+  """
+  Check if a specific email message is a promotion email using the Gmail API.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  message_id (str): The ID of the email message to check.
+
+  Returns:
+  bool: True if the email is a promotion, False otherwise.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    message = service.users().messages().get(userId="me", id=message_id).execute()
+    labels = message["labelIds"]
+
+    PROMOTIONAL_SENDERS_FILE = config.PROMOTIONAL_SENDERS_FILE
+    PROMOTIONAL_SUBJECTS_FILE = config.PROMOTIONAL_SUBJECTS_FILE
+    PROMOTIONAL_DOMAINS_FILE = config.PROMOTIONAL_DOMAINS_FILE
+
+    sender = get_sender_email(creds, message_id)
+    subject = get_email_subject(creds, message_id)
+    domain = get_email_domain(creds, message_id)
+    if sender and subject and domain:
+      promotional_senders = utils.read_file(PROMOTIONAL_SENDERS_FILE)
+      promotional_subjects = utils.read_file(PROMOTIONAL_SUBJECTS_FILE)
+      promotional_domains = utils.read_file(PROMOTIONAL_DOMAINS_FILE)
+
+      # Check if the email has any promotional keywords in the sender, subject, or domain
+      if any(keyword.lower() in sender.lower() for keyword in promotional_senders):
+        print(f"Sender: {sender} is a promotional sender.")
+        return True
+      if any(keyword.lower() in subject.lower() for keyword in promotional_subjects):
+        print(f"Subject: {subject} is a promotional subject.")
+        return True
+      if any(keyword.lower() in domain.lower() for keyword in promotional_domains):
+        print(f"Domain: {domain} is a promotional domain.")
+        return True
+      
+    # Check if the email has the "CATEGORY_PROMOTIONS" label
+    if "CATEGORY_PROMOTIONS" in labels:
+      return True
+
+    return False
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while checking for promotion email : {error}")
+    return False
+  except Exception as e:
+    print(f"An error occurred while checking for promotion email : {e}")
+    return False
+  
+def get_label_id(creds, label_name):
+  """
+  Get the ID of a specific label using the Gmail API.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  label_name (str): The name of the label to retrieve.
+
+  Returns:
+  str: The ID of the label if found, otherwise None.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().labels().list(userId="me").execute()
+    labels = results.get("labels", [])
+
+    for label in labels:
+      if label["name"] == label_name:
+        return label["id"]
+    return None
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while getting label ID : {error}")
+    return None
+  except Exception as e:
+    print(f"An error occurred while getting label ID : {e}")
+    return None
+  
+def apply_label(creds, message_id, label_name):
+  """
+  Apply a label to a specific email message using the Gmail API.
+  This function modifies the email message by adding the specified label ID to it.
+
+  Parameters:
+  creds (Credentials): The credentials object containing the user's access and refresh tokens.
+  message_id (str): The ID of the email message to modify.
+  label_name (str): The name of the label to apply to the email message.
+
+  Returns:
+  bool: True if the label was successfully applied, False otherwise.
+  """
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    label_id = get_label_id(creds, label_name)
+
+    if label_id:
+      # Apply the label to the email message
+      service.users().messages().modify(
+        userId="me",
+        id=message_id,
+        body={"addLabelIds": [label_id]}
+      ).execute()
+      print(f"Label '{label_name}' applied to message ID: {message_id}")
+      return True
+    else:
+      print(f"Label '{label_name}' not found.")
+      return False
+
+  except HttpError as error:
+    print(f"An HTTP error occurred while applying label : {error}")
+    return False
+  except Exception as e:
+    print(f"An error occurred while applying label : {e}")
     return False
--- src/test.py
+++ src/test.py
@@ -1,14 +1,49 @@
-import email
 import mail_handler
+import os
 
-mail = mail_handler.gmail_connect()
-if mail:
-    print("Connected to Gmail successfully.")
-else:
-    print("Failed to connect to Gmail.")
+TARGET_FOLDER = mail_handler.config.TARGET_FOLDER  # Folder to move promotional emails to
 
-subject = "Vous avez téléchargé le modèle Student OS depuis Notion"
-sender = "marketplace@mail.notion.so"
+try:
+    creds = mail_handler.start_gmail_api()
 
-print(mail_handler.is_promotional(subject, sender))  # Expected output: True
-print(mail_handler.is_promotional("Hello World", ""))  # Expected output: False
+    if creds:
+
+        messages = mail_handler.get_unread_emails(creds)
+        promos = 0
+
+        if messages:
+            for message in messages:
+                sender = mail_handler.get_sender_email(creds, message["id"])
+                subject = mail_handler.get_email_subject(creds, message["id"])
+                domain = mail_handler.get_email_domain(creds, message["id"])
+                if sender and subject and domain:
+                    is_promo = mail_handler.is_promo_email(creds, message["id"])
+                    if is_promo:
+                        print(f"Promo email detected :\n")
+                        print(f"Subject: {subject}")
+                        print(f"Sender: {sender}")
+                        print(f"Domain: {domain}")
+                        promos += 1
+                        # Move the email to the promotional folder
+                        moved = mail_handler.apply_label(creds, message["id"], TARGET_FOLDER)
+                        if moved:
+                            print(f"Email moved to {TARGET_FOLDER} folder.")
+                        else:
+                            print(f"Failed to move email to {TARGET_FOLDER} folder.")
+                    else:
+                        print(f"Non-promo email detected :\n")
+                        print(f"Subject: {subject}")
+                        print(f"Sender: {sender}")
+                        print(f"Domain: {domain}")
+                else:
+                    print("Failed to retrieve email details.")
+                    continue
+            mail_handler.utils.display_summary(len(messages), promos)
+        else:
+            print("No unread emails found.")
+except KeyboardInterrupt:
+    print("Process interrupted by user.")
+    os._exit(0)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    os._exit(1)
